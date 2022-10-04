99f4c696f275266f6d881e0cb1a75e13e06adaf0
unknown <vishakh.mallesh@toyokoh.com>
2022-10-04 11:31:12 +0530

    commit

--- src/components/editor/CodeEditorWindow.js
+++ src/components/editor/CodeEditorWindow.js
@@ -1,27 +1,37 @@
 // CodeEditorWindow.js
 
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 
 import Editor from "@monaco-editor/react";
 
-const CodeEditorWindow = ({ onChange, language, code, theme, viewHorizontal }) => {
+const CodeEditorWindow = ({
+  onChange,
+  language,
+  code,
+  theme,
+  viewHorizontal,
+}) => {
   const [value, setValue] = useState(code || "");
 
   const handleEditorChange = (value) => {
     setValue(value);
     onChange("code", value);
   };
-  
+
   return (
-    <Editor
-      height={viewHorizontal ? "calc(100vh - 100px - 3rem)" : "calc(50vh - 100px)"}
-      width={`100%`}
-      language={language}
-      value={value}
-      theme={theme}
-      defaultValue="// some comment"
-      onChange={handleEditorChange}
-    />
+    <Suspense fallback={<div>Loading</div>}>
+      <Editor
+        height={
+          viewHorizontal ? "calc(100vh - 100px - 3rem)" : "calc(50vh - 100px)"
+        }
+        width={`100%`}
+        language={language}
+        value={value}
+        theme={theme}
+        defaultValue="// some comment"
+        onChange={handleEditorChange}
+      />
+    </Suspense>
   );
 };
 export default CodeEditorWindow;
